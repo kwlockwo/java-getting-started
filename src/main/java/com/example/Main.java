04046a244f95dd8ddfe4fb443d55b4fbda1665eb
Keith Lockwood <kwlockwo@gmail.com>
2019-04-23 11:01:31 +1000

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -53,6 +53,11 @@ public class Main {
     return "index";
   }
 
+  @RequestMapping("/comingsoon")
+  String comingsoon() {
+    return "comingsoon";
+  }
+  
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
